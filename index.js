39e0e224b551d56f4fa426c77ce43b84a1944452
Ngecha <ngechadennis@gmail.com>
2024-07-05 23:44:09 +0300

    assignment

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
+let num1=2;
+let num2=31;
+let multiply=num1*num2;
 
-// Write your code here
+const random =Math.floor(Math.random() * 10) + 1;
+console.log(random);
+
+let num3=69
+let num4=5
+let mod=num3%num4
+console.log(mod);
+
+let max = Math.max(20,6,5,12,2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Write your code here.
